67b3740cbbe36c34a29ebe37631b0388aa71caa9
baconsocrispy <68571497+baconsocrispy@users.noreply.github.com>
2024-04-06 10:27:28 -0700

    feature AB#131: added nextJest configuration to jest.config and test:watch script to package.json

--- jest.config.ts
+++ jest.config.ts
@@ -2,8 +2,14 @@
  * For a detailed explanation regarding each configuration property, visit:
  * https://jestjs.io/docs/configuration
  */
-
 import type { Config } from 'jest';
+import nextJest from 'next/jest.js';
+
+const createJestConfig = nextJest({
+    // Provide the path to your Next.js app to load next.config.js and .env 
+    // files in your test environment
+    dir: './',
+});
 
 const config: Config = {
   // All imported modules in your tests should be mocked automatically
@@ -102,7 +108,7 @@ const config: Config = {
   // notifyMode: "failure-change",
 
   // A preset that is used as a base for Jest's configuration
-  preset: 'ts-jest',
+  // preset: undefined,
 
   // Run tests from one or more projects
   // projects: undefined,
@@ -137,7 +143,7 @@ const config: Config = {
   // setupFiles: [],
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
-  setupFilesAfterEnv: [ "<rootDir>/jest-setup.ts" ],
+  // setupFilesAfterEnv: [],
 
   // The number of seconds after which a test is considered as slow and reported as such in the results.
   // slowTestThreshold: 5,
@@ -196,4 +202,4 @@ const config: Config = {
   // watchman: true,
 };
 
-export default config;
+export default createJestConfig(config);
